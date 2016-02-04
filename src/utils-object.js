ffbc035884ae185a3b583e5a1174b967368cd04d
Tim Davis <thebearingedge@gmail.com>
2016-02-04 00:22:15 -0800

    export const

--- src/utils-object.js
+++ src/utils-object.js
@@ -1,7 +1,7 @@
 
-export let equalForKeys = (a = {}, b = {}) =>
+export const equalForKeys = (a = {}, b = {}) =>
 
   Object.keys(a).every(key => a[key] === b[key])
 
 
-export let isFunction = val => typeof val === 'function'
+export const isFunction = val => typeof val === 'function'
