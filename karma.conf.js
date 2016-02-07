bb1066dbc78346205cdb5b2cb4ad3de9c174ccf8
Tim Davis <thebearingedge@gmail.com>
2016-02-06 21:11:43 -0800

    clean up dev stuff

--- karma.conf.js
+++ karma.conf.js
@@ -1,62 +1,58 @@
 
-module.exports = config => {
+module.exports = config => config.set({
 
-  config.set({
+  frameworks: ['browserify', 'mocha'],
 
-    frameworks: ['browserify', 'mocha'],
+  plugins: [
+    'karma-browserify',
+    'karma-jsdom-launcher',
+    'karma-mocha',
+    'karma-mocha-reporter',
+    'karma-coverage'
+  ],
 
-    plugins: [
-      'karma-browserify',
-      'karma-jsdom-launcher',
-      'karma-mocha',
-      'karma-mocha-reporter',
-      'karma-coverage'
-    ],
-
-    files: [
-      'node_modules/babel-polyfill/dist/polyfill.js',
-      {
-        pattern: 'test/**/*.test.js',
-        watched: false, included: true, served: true
-      },
-      {
-        pattern: 'src/**/*.js',
-        watched: false, included: true, served: true
-      }
-    ],
-
-    preprocessors: {
-      'test/**/*.test.js': ['browserify'],
-      'src/**/*.js': ['browserify']
+  files: [
+    'node_modules/babel-polyfill/dist/polyfill.js',
+    {
+      pattern: 'test/**/*.test.js',
+      watched: false, included: true, served: true
     },
+    {
+      pattern: 'src/**/*.js',
+      watched: false, included: true, served: true
+    }
+  ],
 
-    browserify: {
-      debug: true,
-      transform: [
-        ['babelify', {
-          presets: ['es2015', 'stage-2'],
-          plugins: [
-            ['transform-react-jsx', { pragma: 'h' }]
-          ],
-          sourceMaps: 'inline'
-        }],
-        ['browserify-babel-istanbul', {
-        }]
-      ]
-    },
+  preprocessors: {
+    'test/**/*.test.js': ['browserify'],
+    'src/**/*.js': ['browserify']
+  },
 
-    reporters: ['mocha'],
+  browserify: {
+    debug: true,
+    transform: [
+      ['babelify', {
+        presets: ['es2015', 'stage-2'],
+        plugins: [
+          ['transform-react-jsx', { pragma: 'h' }]
+        ],
+        sourceMaps: 'inline'
+      }],
+      ['browserify-babel-istanbul', {
+      }]
+    ]
+  },
 
-    mochaReporter: { output: 'autowatch' },
+  reporters: ['mocha'],
 
-    port: 9876,
+  mochaReporter: { output: 'autowatch' },
 
-    autoWatchBatchDelay: 0,
+  port: 9876,
 
-    logLevel: config.LOG_ERROR,
+  autoWatchBatchDelay: 0,
 
-    browsers: ['jsdom']
+  logLevel: config.LOG_ERROR,
 
-  })
+  browsers: ['jsdom']
 
-}
+})
