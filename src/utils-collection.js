1848918157ee769412bec693d54c77ca5b90ecdc
Tim Davis <thebearingedge@gmail.com>
2016-02-08 23:32:35 -0800

    functional?

--- src/utils-collection.js
+++ src/utils-collection.js
@@ -1,46 +1,37 @@
 
+export const take = (count, array) => array.slice(0, count)
+
+
+export const head = array => array[0]
+
+
+export const tail = array => array.slice(1)
+
+
+export const end = array => array.length - 1
+
+
+export const last = array => array[end(array)]
+
+
+export const init = array => take(end(array), array)
+
+
 export const compact = array => array.filter(Boolean)
 
 
 export const flatMap = (array, cb) => [].concat(...array.map(cb))
 
 
-export const takeWhile = (array, test) => {
+export const takeWhile = (array, test) =>
 
-  const taken = []
-  const { length } = array
-
-  if (!length) return taken
-
-  let i = 0
-
-  while (test(array[i])) {
-
-    taken.push(array[i])
-
-    i++
-  }
-
-  return taken
-}
+  test(head(array))
+    ? [head(array), ...takeWhile(tail(array), test)]
+    : []
 
 
-export const takeRightWhile = (array, test) => {
+export const takeRightWhile = (array, test) =>
 
-  const taken = []
-  const { length } = array
-
-  if (!length) return taken
-
-  let i = length - 1
-
-  while (i > -1 && test(array[i])) {
-
-    taken.unshift(array[i])
-
-    i--
-  }
-
-  return taken
-}
-
+  test(last(array))
+    ? [...takeRightWhile(init(array), test), last(array)]
+    : []
--- test/utils-collection.test.js
+++ test/utils-collection.test.js
@@ -2,7 +2,7 @@
 import { expect } from 'chai'
 import { compact, takeWhile, takeRightWhile } from '../src/utils-collection'
 
-describe('utils-collection', () => {
+describe.only('utils-collection', () => {
 
   describe('compact(collection)', () => {
 
@@ -19,9 +19,9 @@ describe('utils-collection', () => {
   describe('takeWhile(collection, test)', () => {
 
     it('takes until test is false', () => {
-      const collection = [0, 1, 2]
+      const collection = [-1, 0, 1, 2]
       const taken = takeWhile(collection, item => item <= 1)
-      expect(taken).to.deep.equal([0, 1])
+      expect(taken).to.deep.equal([-1, 0, 1])
     })
 
     it('returns an empty array', () => {
@@ -35,9 +35,9 @@ describe('utils-collection', () => {
   describe('takeRightWhile(collection, test)', () => {
 
     it('takes from end of collection until test is false', () => {
-      const collection = [0, 1, 2]
+      const collection = [0, 1, 2, 3]
       const taken = takeRightWhile(collection, item => item > 0)
-      expect(taken).to.deep.equal([1, 2])
+      expect(taken).to.deep.equal([1, 2, 3])
     })
 
     it('returns an empty array', () => {
